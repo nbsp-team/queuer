a14af910bf80747d78e1001110c34144a16e4132
Nickolay <lukaville@gmail.com>
2015-12-21 20:51:20 +0300

    gcm

--- app/src/main/java/com/nbsp/queuer/App.java
+++ app/src/main/java/com/nbsp/queuer/App.java
@@ -1,19 +1,39 @@
 package com.nbsp.queuer;
 
 import android.app.Application;
+import android.os.Bundle;
 
 import com.nbsp.queuer.db.DB;
+
+import eu.inloop.easygcm.EasyGcm;
+import eu.inloop.easygcm.GcmListener;
 
 
 /**
  * Класс, описывающий поля и методы типа App.
  */
-public class App extends Application {
+public class App extends Application implements GcmListener {
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         DB.createInstance(this);
+
+//        if (AccountUtils.getInstance(this).isAuthorized()) {
+//            initGcm();
+//        }
+    }
+
+    public void initGcm() {
+        EasyGcm.init(this);
+    }
+
+    @Override
+    public void onMessage(String s, Bundle bundle) {
+    }
+
+    @Override
+    public void sendRegistrationIdToBackend(String s) {
     }
 }
